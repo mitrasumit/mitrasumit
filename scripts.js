d3ea4f1a012e952291d3ab34e5d0d5d08a696e83
Sumit Mitra <mitra.sumit@outlook.com>
2024-02-16 01:30:00 +0530

    Added visibilty hidden onclick

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var landing_page=document.getElementsByClassName("landing-page")[0];
+
+function togglePage() {
+    if(landing_page.style.visibility!='hidden')
+    {
+        landing_page.style.visibility='hidden'
+    }
+}
